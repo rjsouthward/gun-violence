590af4fd24500561d2361180ef004a3c3b4fa813
rjsouthward <rysouth32@gmail.com>
2022-08-01 15:10:17 -0500

    succesfully create rayshader 3d plot in shiny

---
+++ 3d/app.R
@@ -0,0 +1,58 @@
+library(shiny)
+library(tidyverse)
+library(usmap)
+library(rgl)
+library(rayshader)
+library(sf)
+
+setwd("~/Desktop/Personal-Projects/gun-violence")
+options(rgl.useNULL = FALSE)
+
+data <- read_rds("data/consolidated/combined2010to2020.rds")
+
+filtered1 <- data |>
+  filter(! Recovery_State %in% c("VIRGIN ISLANDS", "TOTAL", "GUAM", "CANADA")) |>
+  filter(Source_State != "TOTAL") |>
+  filter(Recovery_State != Source_State) |>
+  group_by(Year, Recovery_State) |>
+  filter(Guns_Recovered > quantile(Guns_Recovered, probs = .9)) |>
+  ungroup() |>
+  pivot_wider(names_from = Year, values_from = Guns_Recovered, values_fill = 0) |>
+  arrange(Recovery_State)
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "state",
+                  label = "Select state:",
+                  choices = as.list(unique(filtered1$Recovery_State))),
+      selectInput(inputId = "year",
+                  label = "Select year:",
+                  choices = as.list(seq(from = 2010, to = 2020))),
+      submitButton("Submit")
+    ),
+    mainPanel(
+      plotOutput(outputId = "recoStateTop10")
+    )
+  )
+)
+
+server <- function(input, output){
+  output$recoStateTop10 <- renderPlot({
+    
+    filtered2 <- filtered1 |>
+      filter(Recovery_State == input$state) |>
+      select(Source_FIPS, as.character(input$year)) |>
+      rename("fips" = "Source_FIPS", "values" = as.character(input$year))
+    
+    map <- plot_usmap(data = filtered2, regions = "states") +
+      scale_fill_viridis_c()
+    
+    plot_gg(map, shadow_intensity = .85, offset_edges = TRUE, raytrace = TRUE, sunangle = 135, solidcolor = "ghostwhite", 
+            theta = 0, phi = 35)
+    
+    render_snapshot(clear = TRUE)
+  })
+}
+
+shinyApp(ui = ui, server = server)
--- Complie-Data.R
+++ Complie-Data.R
@@ -1,21 +1,50 @@
+#Load neccesary packages. 
 library(tidyverse)
 library(janitor)
 library(readxl)
+library(cdlTools)
 
 for (year in 2010:2020){
+  #Read in xl file that lives on my computer. They can be found on the ATF site 
+  # (https://www.atf.gov/resource-center/firearms-trace-data-2020), file called "Number of Firearms Sourced and Recovered in the United States and Territories"
+  
+  #First row of each file is "Recovery State", and first col is "Source State"
+  #Skip the first row while reading and later deselect first col to remove junk.
   read_excel(paste("data/original_atf_data/sourcerecovery_by_state_cy", year, ".xlsx", sep = ""), skip = 1) |>
   select(-1) |>
   rename("Source_State" = "...2") |>
+  #Remove footnotes
   head(55) |>
+  #I wish to treat Source State as the dependent variable, so I transposed the dataframe.
   t() |>
   as.data.frame() |>
+  #Fix generic col names caused by transposing. 
   row_to_names(row = 1) |>
   rownames_to_column("Recovery_State") |>
+  #Convert data to long format for ease of combining. 
   pivot_longer(names_to = "Source_State", values_to = "Guns_Recovered", cols = -Recovery_State) |>
+  mutate(Guns_Recovered = as.integer(Guns_Recovered)) |>
   mutate(Year = year) |>
+  #Create 11 seperate files, one for each year 2010 through 2020, with the uncleaned, uncombined data. 
   write_rds(paste("data/wrangled_by_state/state-recovery", year, ".rds", sep = "")) 
 }
 
+#Combine each of the 11 seperate dataframes, one for each year between 2010 and 2020. 
 condensed <- bind_rows(read_rds("data/wrangled_by_state/state-recovery2010.rds"), read_rds("data/wrangled_by_state/state-recovery2011.rds"), read_rds("data/wrangled_by_state/state-recovery2012.rds"), read_rds("data/wrangled_by_state/state-recovery2013.rds"), read_rds("data/wrangled_by_state/state-recovery2014.rds"), read_rds("data/wrangled_by_state/state-recovery2015.rds"), read_rds("data/wrangled_by_state/state-recovery2016.rds"), read_rds("data/wrangled_by_state/state-recovery2017.rds"), read_rds("data/wrangled_by_state/state-recovery2018.rds"), read_rds("data/wrangled_by_state/state-recovery2019.rds"), read_rds("data/wrangled_by_state/state-recovery2020.rds"))
 
+#Some State names appear differently between annual reports. This code is meant to standardize them. 
+condensed[condensed == "TOTALS"] <- "TOTAL"
+#Worth noting that Guam refers to both Guam and the Northern Marina Islands.
+condensed[condensed == "GUAM & NORTHERN MARIANA ISLANDS"] <- "GUAM"
+condensed[condensed == "US VIRGIN  ISLANDS" | condensed == "US VIRGIN ISLND" | condensed == "US VIRGIN ISLANDS"] <- "VIRGIN ISLANDS"
+condensed[condensed == "DST OF COLUMBIA"] <- "DISTRICT OF COLUMBIA"
+
+#Creation of a FIPS code column that may be helpful for mapping. 
+condensed <- condensed |>
+  mutate("Source_FIPS" = cdlTools::fips(Source_State, to = "FIPS"))
+
+#cdlTools does not include Virgin Islands FIPS, so do it manually. 
+condensed$Source_FIPS <- ifelse(condensed$Source_State == "VIRGIN ISLANDS", 78, condensed$Source_FIPS)
+
+#Write final, condensed dataset. 
 write_rds(condensed, "data/consolidated/combined2010to2020.rds")
