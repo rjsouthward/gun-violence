ea34c140126d172c405a739ca97e38ab11ebbc8c
rjsouthward <rysouth32@gmail.com>
2022-12-26 11:30:22 -1000

    redirecterize content, now 3 visualizations: 3d rayshader, flow chart, and dot density

--- point/app.R
+++ point/app.R
@@ -1,9 +1,10 @@
+library(shiny)
 library(tidyverse)
 library(urbnmapr)
 
 #Import data
 setwd("~/Desktop/Personal-Projects/gun-violence")
-data <- condensed |>
+data <- read_rds("data/consolidated/combined2010to2020.rds") |>
   #Change 'Total' (name attributed by ATF) to United States throughout df
   mutate(Recovery_State = if_else(Recovery_State == 'Total', 'United States', Recovery_State)) |>
   mutate(Source_State = if_else(Source_State == 'Total', 'United States', Source_State))
